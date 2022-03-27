6fcbb436c7f8a80e2fda852f64c10497097e030c
Mehdi Jafarzadeh <mehdi.jafarzadeh@usu.com>
2022-03-28 00:59:10 +0200

    add welcome branch

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringitApplication.class, args);}
+		SpringApplication.run(SpringitApplication.class, args);
+		System.out.println("Welcome to Springit");
+	}
 
 }
