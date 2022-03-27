6844dbfc686044abeefa96dd396bc70d2eb630d4
Mehdi Jafarzadeh <mehdi.jafarzadeh@usu.com>
2022-03-28 00:53:30 +0200

    update new README.md

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringitApplication.class, args);
-	}
+		SpringApplication.run(SpringitApplication.class, args);}
 
 }
